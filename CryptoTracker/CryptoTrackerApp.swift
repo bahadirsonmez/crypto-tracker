47344ceeae5c7d4de7748b1164a3030179ff3357
Bahadir Sonmez <sonmezbahad@gmail.com>
2022-12-19 16:10:56 +0300

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Bahadır Sönmez on 19.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
