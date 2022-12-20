e0b977d363faef22970e8bc6843f1ff9d347ab1b
Bahadir Sonmez <sonmezbahad@gmail.com>
2022-12-20 21:49:45 +0300

    Episode 5: Create HomeViewModel and bind it to the HomeView by using EnvironmenObject and create a list of CoinRowView

---
+++ CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewModel.swift
+//  CryptoTracker
+//
+//  Created by Bahadır Sönmez on 20.12.2022.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now()+1) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+    
+}
--- CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @EnvironmentObject private var viewModel: HomeViewModel
     @State private var showPortfolio: Bool = false
     
     var body: some View {
@@ -17,8 +19,17 @@ struct HomeView: View {
             
             // MARK: - Content Layer
             VStack() {
-                // MARK: - Header
                 homeHeader
+                
+                columnTitles
+                
+                if !showPortfolio {
+                    allCoinsList
+                        .transition(.move(edge: .leading))
+                } else {
+                    portfolioCoinsList
+                        .transition(.move(edge: .trailing))
+                }
                 
                 Spacer(minLength: 0)
             }
@@ -32,6 +43,7 @@ struct HomeView_Previews: PreviewProvider {
             HomeView()
                 .navigationBarHidden(true)
         }
+        .environmentObject(dev.homeVM)
     }
 }
 
@@ -57,4 +69,40 @@ extension HomeView {
         }
         .padding(.horizontal)
     }
+    
+    private var allCoinsList: some View {
+        List {
+            ForEach(viewModel.allCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(.plain)
+    }
+    
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(viewModel.portfolioCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: true)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(.plain)
+    }
+    
+    private var columnTitles: some View {
+        HStack {
+            Text("Coin")
+            Spacer()
+            if showPortfolio {
+                Text("Holdings")
+            }
+            Text("Price")
+                .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+        }
+        .font(.caption)
+        .foregroundColor(Color.theme.secondaryText)
+        .padding(.horizontal)
+    }
+
 }
