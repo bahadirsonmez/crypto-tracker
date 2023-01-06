691a00eb49ec591b58a0d60c148ffdd88944fa63
Bahadir Sonmez <sonmezbahad@gmail.com>
2023-01-06 18:37:06 +0300

    Episode 23: Create settings view and add data about app, developer and other things

--- CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -12,6 +12,7 @@ struct HomeView: View {
     @EnvironmentObject private var viewModel: HomeViewModel
     @State private var showPortfolio: Bool = false // animate right
     @State private var showPortfolioView: Bool = false // new sheet
+    @State private var showSettingsView: Bool = false // new sheet
     
     @State private var selectedCoin: CoinModel? = nil
     @State private var showDetailView: Bool = false
@@ -45,6 +46,10 @@ struct HomeView: View {
                 
                 Spacer(minLength: 0)
             }
+            .sheet(isPresented: $showSettingsView) {
+                SettingsView()
+            }
+
         }
         .background(
             NavigationLink(destination: DetailLoadingView(coin: $selectedCoin),
@@ -72,6 +77,8 @@ extension HomeView {
                 .onTapGesture {
                     if showPortfolio {
                         showPortfolioView.toggle()
+                    } else {
+                        showSettingsView.toggle()
                     }
                 }
                 .background(CircleButtonAnimationView(animate: $showPortfolio))
---
+++ CryptoTracker/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,119 @@
+//
+//  SettingsView.swift
+//  CryptoTracker
+//
+//  Created by Bahadır Sönmez on 4.01.2023.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    let defaultURL = URL(string: "htttps://www.google.com")!
+    let personelURL = URL(string: "https://bahadirsonmez.github.io")!
+    let coingeckoURL = URL(string: "https://www.coingecko.com")!
+    
+    var body: some View {
+        NavigationView {
+            List {
+                appInfoSection
+                coinGeckoSection
+                developerSection
+                applicationSection
+            }
+            .font(.headline)
+            .accentColor(.blue)
+            .listStyle(.grouped)
+            .navigationTitle("Settings")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    xMarkButton
+                }
+            }
+        }
+
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
+
+extension SettingsView {
+    
+    private var appInfoSection: some View {
+        Section(header: Text("Crypto Tracker")) {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app was made by following a @SwiftfulThinking course on Youtube. It usese MVVM Architecture, Combine and CoreData!")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+        }
+    }
+    
+    private var coinGeckoSection: some View {
+        Section(header: Text("Coin Gecko")) {
+            VStack(alignment: .leading) {
+                Image("coingecko")
+                    .resizable()
+                    .frame(height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("The cryptocurrency data that is used in this app comes from a free API from CoinGecko! Prices may be slightly delayed.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Coingecko Website", destination: coingeckoURL)
+        }
+    }
+    
+    private var developerSection: some View {
+        Section(header: Text("Developer")) {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app is developed by Bahadir Sonmez. It uses SwiftUI and is written 100% in Swift. The project benefits from multi-threading, publisher/subscribers and data persistance.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("My Personel Website", destination: personelURL)
+        }
+    }
+    
+    private var applicationSection: some View {
+        Section(header: Text("Application")) {
+            Link("Terms of Service", destination: defaultURL)
+            Link("Privacy Policy", destination: defaultURL)
+            Link("Company Website", destination: defaultURL)
+            Link("Learn More", destination: defaultURL)
+        }
+    }
+
+    
+}
+
+extension SettingsView {
+    private var xMarkButton: some View {
+        Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }, label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+        })
+    }
+}
